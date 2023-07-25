779fe2e52d61a46732c8a6f241f2c07a9eecc345
rockchen <rochchen@htsc.com>
2023-07-25 21:21:23 +0800

    update

--- src/main/java/com/hthk/fintech/service/basic/AbstractService.java
+++ src/main/java/com/hthk/fintech/service/basic/AbstractService.java
@@ -1,12 +1,11 @@
 package com.hthk.fintech.service.basic;
 
-import com.hthk.fintech.model.event.IEvent;
 import com.hthk.fintech.structure.utils.JacksonUtils;
 
 public abstract class AbstractService {
 
-    protected String ymlStr(IEvent event) {
-        return JacksonUtils.toYMLPrettyTry(event);
+    protected <T> String ymlStr(T model) {
+        return JacksonUtils.toYMLPrettyTry(model);
     }
 
 }
