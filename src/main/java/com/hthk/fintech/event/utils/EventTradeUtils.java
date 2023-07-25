1a4dae8dba97b9b4653a721e4e8ae5d23fc91d3b
rockchen <rochchen@htsc.com>
2023-07-25 20:31:03 +0800

    update

--- src/main/java/com/hthk/fintech/event/utils/EventTradeUtils.java
+++ src/main/java/com/hthk/fintech/event/utils/EventTradeUtils.java
@@ -1,12 +1,15 @@
 package com.hthk.fintech.event.utils;
 
+import com.hthk.fintech.enumration.EventTypeTradeEnum;
 import com.hthk.fintech.event.utils.basic.AbstractEventUtils;
+import com.hthk.fintech.model.event.EventTrade;
 
 import java.time.LocalDateTime;
 
 public class EventTradeUtils extends AbstractEventUtils {
 
-//    public static EventTrade build(String domain, EventTypeTradeEnum eventTypeTrade, LocalDateTime eventTime) {
-//
-//    }
+    public static EventTrade build(String domain, EventTypeTradeEnum eventTypeTrade, LocalDateTime eventTime) {
+        return null;
+    }
+
 }
