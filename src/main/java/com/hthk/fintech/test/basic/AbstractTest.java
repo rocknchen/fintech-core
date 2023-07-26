98f3df572f242707b020783874dc03520184c87b
rockchen <rochchen@htsc.com>
2023-07-26 19:46:35 +0800

    update

--- src/main/java/com/hthk/fintech/test/basic/AbstractTest.java
+++ src/main/java/com/hthk/fintech/test/basic/AbstractTest.java
@@ -1,18 +1,23 @@
 package com.hthk.fintech.test.basic;
 
-import com.hthk.fintech.model.event.IEvent;
 import com.hthk.fintech.structure.utils.JacksonUtils;
 import org.junit.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDateTime;
+
 public abstract class AbstractTest {
 
     protected Logger logger;
 
+    protected LocalDateTime currentTime;
+
     @Before
     public void setUp() {
+
         logger = getLogger();
+        currentTime = LocalDateTime.now();
     }
 
     protected void log(String logFormat, String msg1, String msg2) {
--- src/test/java/com/hthk/fintech/event/dao/impl/EventDAOLocalFileImplTest.java
+++ src/test/java/com/hthk/fintech/event/dao/impl/EventDAOLocalFileImplTest.java
@@ -1,29 +1,20 @@
 package com.hthk.fintech.event.dao.impl;
 
+import com.hthk.fintech.enumration.EventTypeTradeEnum;
 import com.hthk.fintech.model.event.EventTrade;
-import com.hthk.fintech.model.event.IEvent;
 import com.hthk.fintech.test.EventTradeTest;
-import com.hthk.fintech.test.basic.AbstractEventTest;
-import com.hthk.fintech.test.basic.AbstractTest;
-import org.junit.Before;
 import org.junit.Test;
-
-import static com.hthk.fintech.config.FintechStaticData.LOG_FORMAT_ONE_MODEL;
-import static org.junit.Assert.*;
 
 public class EventDAOLocalFileImplTest extends EventTradeTest {
 
-    @Override
-    public void setUp() {
-
-        super.setUp();
-        event = new EventTrade();
-    }
+    private final String DOMAIN = "CALYPSO";
 
     @Test
     public void testSaveEvent_TRADE_REFRESH_CALYPSO() {
 
-//        event
+        EventTypeTradeEnum type = EventTypeTradeEnum.REFRESH;
+
+//        event = EventTrade.newInstance(DOMAIN, type, currentTime);
         logYML(event);
     }
 
