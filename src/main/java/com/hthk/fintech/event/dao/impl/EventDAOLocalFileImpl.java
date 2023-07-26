b59234553248776dd5bc8ac73aa289ac01de0ecf
rockchen <rochchen@htsc.com>
2023-07-26 21:11:45 +0800

    update

--- src/main/java/com/hthk/fintech/event/dao/impl/EventDAOLocalFileImpl.java
+++ src/main/java/com/hthk/fintech/event/dao/impl/EventDAOLocalFileImpl.java
@@ -6,8 +6,10 @@ import com.hthk.fintech.exception.AttributeEmptyException;
 import com.hthk.fintech.exception.PersistenceException;
 import com.hthk.fintech.model.event.IEvent;
 import com.hthk.fintech.service.basic.AbstractService;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.time.LocalDateTime;
 
 import static com.hthk.fintech.config.FintechStaticData.KW_DATE_MONTH;
@@ -20,13 +22,28 @@ public class EventDAOLocalFileImpl extends AbstractService implements EventDAO {
     public void save(IEvent event) throws PersistenceException, AttributeEmptyException {
 
         LocalDateTime eventTime = event.getTime();
-        logTime(eventTime, KW_EVENT_TIME);
+        logTime(eventTime, LogLevel.DEBUG, KW_EVENT_TIME);
 
         String monthStr = LocalDateTimeUtils.format("yyyyMM", eventTime);
         logStr(monthStr, KW_DATE_MONTH);
+
+        File eventFile = getEventFile(monthStr);
 //        logYML();
 //        File eventFile =
     }
 
+    private File getEventFile(String monthStr) {
+
+        String eventFileFolder = appConfig.getLocalFileRootFolder();
+        logStr(eventFileFolder, LogLevel.DEBUG, null);
+
+        String eventFileName = null;
+        logStr(eventFileName, LogLevel.DEBUG, null);
+
+        String eventFilePath = eventFileFolder + "/" + eventFileName;
+        logStr(eventFilePath, LogLevel.DEBUG, null);
+
+        return null;
+    }
 
 }
--- src/main/java/com/hthk/fintech/service/basic/AbstractService.java
+++ src/main/java/com/hthk/fintech/service/basic/AbstractService.java
@@ -1,8 +1,11 @@
 package com.hthk.fintech.service.basic;
 
+import com.hthk.fintech.config.AppConfig;
 import com.hthk.fintech.structure.utils.JacksonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
@@ -12,6 +15,9 @@ import java.util.Optional;
 import static com.hthk.fintech.config.FintechStaticData.DEFAULT_DATE_TIME_FORMAT;
 
 public abstract class AbstractService {
+
+    @Autowired
+    protected AppConfig appConfig;
 
     protected Logger getLogger() {
         return LoggerFactory.getLogger(this.getClass());
@@ -30,18 +36,40 @@ public abstract class AbstractService {
     }
 
     protected <T> void logStr(T model, String msg) {
+        logStr(model, LogLevel.INFO, msg);
+    }
+
+    protected <T> void logStr(T model, LogLevel level, String msg) {
         String logMsg = StringUtils.hasText(msg) ? msg + ": {}" : "{}";
-        getLogger().info(logMsg, model);
+        switch (level) {
+            case DEBUG:
+                getLogger().debug(logMsg, model);
+                break;
+            default:
+                getLogger().info(logMsg, model);
+                break;
+        }
     }
 
     protected void logTime(LocalDateTime time) {
-        logTime(time, null);
+        logTime(time, LogLevel.INFO, null);
     }
 
     protected void logTime(LocalDateTime time, String msg) {
+        logTime(time, LogLevel.INFO, msg);
+    }
+
+    protected void logTime(LocalDateTime time, LogLevel level, String msg) {
         String timeMsg = Optional.ofNullable(time).map(t -> t.format(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT))).orElse(null);
         String logMsg = StringUtils.hasText(msg) ? msg + ": {}" : "{}";
-        getLogger().info(logMsg, timeMsg);
+        switch (level) {
+            case DEBUG:
+                getLogger().debug(logMsg, timeMsg);
+                break;
+            default:
+                getLogger().info(logMsg, timeMsg);
+                break;
+        }
     }
 
 }
