a4ad5bb64b9a9a9dbdb521b89fb3a1fa897232f9
rockchen <rochchen@htsc.com>
2023-07-26 20:50:31 +0800

    update

--- src/main/java/com/hthk/common/utils/LocalDateTimeUtils.java
+++ src/main/java/com/hthk/common/utils/LocalDateTimeUtils.java
@@ -1,10 +1,16 @@
 package com.hthk.common.utils;
 
+import com.hthk.fintech.exception.AttributeMissingException;
+
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeUtils {
 
     public static String format(String format, LocalDateTime time) {
-//        time.format()
+        if (time == null) {
+//            throw new ()
+        }
+        return time.format(DateTimeFormatter.ofPattern(format));
     }
 }
