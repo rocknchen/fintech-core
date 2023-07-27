3f2cf9af4ef60f8a9bbef6f5c7193e1611ec7fa8
rockchen <rochchen@htsc.com>
2023-07-27 19:55:55 +0800

    update

--- src/main/java/com/hthk/fintech/event/dao/EventDAO.java
+++ src/main/java/com/hthk/fintech/event/dao/EventDAO.java
@@ -4,8 +4,12 @@ import com.hthk.fintech.exception.AttributeEmptyException;
 import com.hthk.fintech.exception.PersistenceException;
 import com.hthk.fintech.model.event.IEvent;
 
+import java.util.List;
+
 public interface EventDAO {
 
     void save(IEvent event) throws PersistenceException, AttributeEmptyException;
+
+    List<IEvent> get(EventCriteria criteria);
 
 }
