5d4a68b8e0263f578c445ec6b5f5527af687d618
rockchen <rochchen@htsc.com>
2023-07-28 10:43:37 +0800

    update

--- src/test/java/com/hthk/fintech/event/dao/impl/EventDAOLocalFileImplTest.java
+++ src/test/java/com/hthk/fintech/event/dao/impl/EventDAOLocalFileImplTest.java
@@ -1,12 +1,15 @@
 package com.hthk.fintech.event.dao.impl;
 
+import com.hthk.fintech.enumration.EventTypeMarketDataEnum;
 import com.hthk.fintech.event.dao.EventDAO;
 import com.hthk.fintech.exception.AttributeEmptyException;
 import com.hthk.fintech.exception.PersistenceException;
+import com.hthk.fintech.model.event.EventMarketData;
 import com.hthk.fintech.model.event.EventTrade;
 import com.hthk.fintech.test.EventTradeTest;
 import org.junit.Test;
 
+import static com.hthk.fintech.enumration.EventTypeMarketDataEnum.VOL_SURFACE;
 import static com.hthk.fintech.enumration.EventTypeTradeEnum.REFRESH;
 
 public class EventDAOLocalFileImplTest extends EventTradeTest {
@@ -31,7 +34,7 @@ public class EventDAOLocalFileImplTest extends EventTradeTest {
     @Test
     public void testSaveEvent_VOL_COMMODITY_0900HKT_READY_CALYPSO() {
 
-        event = EventTrade.newInstance(DOMAIN, REFRESH, null, currentTime);
+//        event = EventMarketData.newInstance(DOMAIN, VOL_SURFACE, READY, currentTime);
         logYML(event);
 //        dao.save(event);
     }
