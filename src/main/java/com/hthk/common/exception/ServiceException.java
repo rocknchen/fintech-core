9f830ef5ac5c4021caf061226f20eea808ddf597
rockchen <rochchen@htsc.com>
2023-07-31 11:19:27 +0800

    update

---
+++ src/main/java/com/hthk/common/exception/ServiceException.java
@@ -0,0 +1,16 @@
+package com.hthk.common.exception;
+
+public class ServiceException extends Exception {
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(
+            String message,
+            Throwable cause
+    ) {
+        super(message, cause);
+    }
+
+}
--- src/main/java/com/hthk/common/utils/CustomReflectionUtils.java
+++ src/main/java/com/hthk/common/utils/CustomReflectionUtils.java
@@ -1,9 +1,24 @@
 package com.hthk.common.utils;
 
+import com.hthk.common.exception.ServiceException;
+
+import java.lang.reflect.Field;
+
 public class CustomReflectionUtils {
 
     public static String getMethodName(String fieldName, String methodStart) {
         return methodStart + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+    }
+
+    public static void setField(Object model, String cp, Object value) throws ServiceException {
+
+        try {
+            Field field = model.getClass().getDeclaredField(cp);
+            field.setAccessible(true);
+            field.set(model, value);
+        } catch (Exception e) {
+            throw new ServiceException(e.getMessage(), e);
+        }
     }
 
 }
