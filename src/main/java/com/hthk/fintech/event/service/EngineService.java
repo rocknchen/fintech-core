55f387b8992080ffd8333007301f1a7921897576
rockchen <rochchen@htsc.com>
2023-08-01 15:50:14 +0800

    update

---
+++ src/main/java/com/hthk/fintech/event/service/EngineService.java
@@ -0,0 +1,11 @@
+package com.hthk.fintech.event.service;
+
+import com.hthk.fintech.model.event.EventProcessEntity;
+
+import java.util.List;
+
+public interface EngineService {
+
+    List<EventProcessEntity> getEntity();
+
+}
