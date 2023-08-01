4c6382be753c570b2921413ff017b4a24e4ce544
rockchen <rochchen@htsc.com>
2023-08-01 16:22:21 +0800

    update

--- src/main/java/com/hthk/fintech/event/service/EventService.java
+++ src/main/java/com/hthk/fintech/event/service/EventService.java
@@ -2,10 +2,16 @@ package com.hthk.fintech.event.service;
 
 import com.hthk.fintech.exception.AttributeEmptyException;
 import com.hthk.fintech.exception.PersistenceException;
+import com.hthk.fintech.model.event.EventCriteria;
 import com.hthk.fintech.model.event.IEvent;
+
+import java.io.IOException;
+import java.util.List;
 
 public interface EventService {
 
     void save(IEvent event) throws AttributeEmptyException, PersistenceException;
+
+    List<IEvent> get(EventCriteria criteria) throws AttributeEmptyException, IOException;
 
 }
--- src/main/java/com/hthk/fintech/event/service/impl/EventServiceDefaultImpl.java
+++ src/main/java/com/hthk/fintech/event/service/impl/EventServiceDefaultImpl.java
@@ -4,6 +4,7 @@ import com.hthk.fintech.event.dao.EventDAO;
 import com.hthk.fintech.exception.AttributeEmptyException;
 import com.hthk.fintech.exception.PersistenceException;
 import com.hthk.fintech.log.utils.LogUtils;
+import com.hthk.fintech.model.event.EventCriteria;
 import com.hthk.fintech.model.event.IEvent;
 import com.hthk.fintech.event.service.EventService;
 import com.hthk.fintech.service.basic.AbstractService;
@@ -11,6 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
 
 import static com.hthk.fintech.config.FintechStaticData.KW_SAVE_EVENT;
 
@@ -27,6 +31,13 @@ public class EventServiceDefaultImpl extends AbstractService implements EventSer
 
         logger.info(LogUtils.wrapStr(KW_SAVE_EVENT), ymlStr(event));
         dao.save(event);
+    }
+
+    @Override
+    public List<IEvent> get(EventCriteria criteria) throws AttributeEmptyException, IOException {
+
+        logYML(criteria);
+        return dao.get(criteria);
     }
 
 }
