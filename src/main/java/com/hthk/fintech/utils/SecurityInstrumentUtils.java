0de786f5fad8ee6b47f8f5afe15fb3ab62cd5918
rockchen <rochchen@htsc.com>
2023-08-10 10:07:22 +0800

    update

--- src/main/java/com/hthk/fintech/utils/SecurityInstrumentUtils.java
+++ src/main/java/com/hthk/fintech/utils/SecurityInstrumentUtils.java
@@ -1,6 +1,7 @@
 package com.hthk.fintech.utils;
 
 import com.hthk.fintech.enumration.InstrumentGroup;
+import com.hthk.fintech.enumration.SecurityGroupEnum;
 import com.hthk.fintech.model.instrument.ISecurity;
 import com.hthk.fintech.model.instrument.Security;
 
@@ -9,7 +10,7 @@ public class SecurityInstrumentUtils {
     public static ISecurity build(String name) {
 
         Security security = new Security();
-        security.setInstrumentGroup(InstrumentGroup.SECURITY);
+        security.setSecurityGroup(SecurityGroupEnum.LISTED.SECURITY);
         security.setName(name);
         return security;
     }
