dda9c77d1969981722fd4e094ac6c7c393928b00
rockchen <rochchen@htsc.com>
2023-08-10 09:49:23 +0800

    update

--- src/main/java/com/hthk/fintech/utils/BasicInstrumentUtils.java
+++ src/main/java/com/hthk/fintech/utils/BasicInstrumentUtils.java
@@ -4,13 +4,14 @@ import com.hthk.fintech.enumration.InstrumentGroup;
 import com.hthk.fintech.model.instrument.IInstrument;
 import com.hthk.fintech.model.instrument.Instrument;
 
-public class CustomizedInstrumentUtils {
+public class BasicInstrumentUtils {
 
     public static IInstrument build(String name) {
 
         Instrument instrument = new Instrument();
-        instrument.setInstrumentGroup(InstrumentGroup.CUSTOMIZATION);
+        instrument.setInstrumentGroup(InstrumentGroup.BASIC);
         instrument.setName(name);
         return instrument;
     }
+
 }
