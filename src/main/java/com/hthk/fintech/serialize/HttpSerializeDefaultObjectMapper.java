80834ee4ba67d3702288df828573b2a08fbc188c
rockchen <rochchen@htsc.com>
2023-11-14 15:33:23 +0800

    update

---
+++ src/main/java/com/hthk/fintech/serialize/HttpSerializeDefaultObjectMapper.java
@@ -0,0 +1,48 @@
+package com.hthk.fintech.serialize;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.hthk.fintech.config.FintechStaticData.DEFAULT_DATE_FORMAT;
+import static com.hthk.fintech.config.FintechStaticData.DEFAULT_DATE_TIME_FORMAT;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/11/14 15:27
+ */
+@Component("httpSerializeDefaultObjectMapper")
+public class HttpSerializeDefaultObjectMapper extends ObjectMapper {
+
+    public HttpSerializeDefaultObjectMapper() {
+
+        super();
+        this.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        this.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        this.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        SimpleModule simpleModule = new SimpleModule()
+                .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)))
+                .addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)))
+                .addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)))
+                .addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)))
+                .addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)))
+                .addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)));
+
+        this.registerModule(simpleModule);
+
+    }
+}
--- src/main/java/com/hthk/fintech/web/http/config/HttpSerializeConfig.java
+++ src/main/java/com/hthk/fintech/web/http/config/HttpSerializeConfig.java
@@ -1,11 +1,30 @@
 package com.hthk.fintech.web.http.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Author: Rock CHEN
  * @Date: 2023/11/14 15:07
  */
+@Configuration
 public class HttpSerializeConfig implements WebMvcConfigurer {
+
+    @Resource(name = "httpSerializeDefaultObjectMapper")
+    private ObjectMapper httpResponseMapper;
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        messageConverter.setObjectMapper(httpResponseMapper);
+        converters.add(0, messageConverter);
+    }
 
 }
