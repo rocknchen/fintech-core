7c8c5deee4a95ed77399105f29572f7e5c33cff0
rockchen <rochchen@htsc.com>
2023-11-14 14:44:51 +0800

    update

---
+++ src/main/java/com/hthk/fintech/utils/HttpResponseUtils.java
@@ -0,0 +1,34 @@
+package com.hthk.fintech.utils;
+
+import com.hthk.fintech.model.web.http.HttpResponse;
+import com.hthk.fintech.model.web.http.HttpStatusCodeEnum;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/11/14 14:38
+ */
+public class HttpResponseUtils {
+
+    public static HttpResponse success() {
+        HttpResponse resp = new HttpResponse(HttpStatusCodeEnum.SUCCESS);
+        return resp;
+    }
+
+    public static <T> HttpResponse success(T data) {
+        HttpResponse resp = new HttpResponse(HttpStatusCodeEnum.SUCCESS);
+        resp.setData(data);
+        return resp;
+    }
+
+    public static HttpResponse fail() {
+        HttpResponse resp = new HttpResponse(HttpStatusCodeEnum.FAIL);
+        return resp;
+    }
+
+    public static <T> HttpResponse fail(T data) {
+        HttpResponse resp = new HttpResponse(HttpStatusCodeEnum.FAIL);
+        resp.setData(data);
+        return resp;
+    }
+
+}
