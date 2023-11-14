ed52561cf8fe547f5c8c948a82f25088207e9605
rockchen <rochchen@htsc.com>
2023-11-14 15:09:19 +0800

    update

---
+++ src/main/java/com/hthk/fintech/web/http/config/HttpSerializeConfig.java
@@ -0,0 +1,11 @@
+package com.hthk.fintech.web.http.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/11/14 15:07
+ */
+public class HttpSerializeConfig implements WebMvcConfigurer {
+
+}
