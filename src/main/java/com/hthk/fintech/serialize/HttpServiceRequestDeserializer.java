6ad6636eca60991e113b67f1d32ac6db36056248
rockchen <rochchen@htsc.com>
2023-12-22 16:09:47 +0800

    update

--- src/main/java/com/hthk/fintech/serialize/HttpServiceRequestDeserializer.java
+++ src/main/java/com/hthk/fintech/serialize/HttpServiceRequestDeserializer.java
@@ -2,15 +2,14 @@ package com.hthk.fintech.serialize;
 
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hthk.fintech.model.web.http.HttpRequestActionTypeEnum;
-import com.hthk.fintech.model.web.http.HttpRequestParams;
-import com.hthk.fintech.model.web.http.HttpServiceRequest;
-import com.hthk.fintech.model.web.http.IRequestAction;
+import com.hthk.fintech.model.web.http.*;
+import com.hthk.fintech.structure.utils.JacksonUtils;
 import org.reflections.Reflections;
 import org.reflections.util.ConfigurationBuilder;
 import org.slf4j.Logger;
@@ -24,6 +23,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static com.hthk.fintech.config.FintechStaticData.DEFAULT_PACKAGE;
+import static com.hthk.fintech.config.FintechStaticData.LOG_DEFAULT;
 
 /**
  * @Author: Rock CHEN
@@ -35,14 +35,14 @@ public class HttpServiceRequestDeserializer<P, C> extends JsonDeserializer<HttpS
 
     private DefaultObjectMapperFactory mapperFactory = new DefaultObjectMapperFactory();
 
-    private static Map<HttpRequestActionTypeEnum, Class<? extends HttpRequestParams>> actionParamMap;
+    private static Map<HttpRequestActionTypeEnum, Class<?>> actionParamMap;
 
     private ObjectMapper objectMapper = mapperFactory.getObjectMapper();
 
     static {
         Reflections reflections = new Reflections(new ConfigurationBuilder().forPackages(DEFAULT_PACKAGE));
-        Set<Class<? extends HttpRequestParams>> supertypeSet = reflections.getSubTypesOf(HttpRequestParams.class);
-        List<Class<? extends HttpRequestParams>> dataCriteriaList = supertypeSet.stream().filter(t -> t.getAnnotation(HttpRequestParams.class) != null).collect(Collectors.toList());
+        Set<Class<?>> supertypeSet = reflections.getTypesAnnotatedWith(HttpRequestParams.class);
+        List<Class<?>> dataCriteriaList = supertypeSet.stream().filter(t -> t.getAnnotation(HttpRequestParams.class) != null).collect(Collectors.toList());
         actionParamMap = dataCriteriaList.stream().collect(Collectors.toMap(t -> {
             HttpRequestParams dataCriteria = t.getAnnotation(HttpRequestParams.class);
             return dataCriteria.name();
@@ -56,15 +56,29 @@ public class HttpServiceRequestDeserializer<P, C> extends JsonDeserializer<HttpS
 
         JsonNode jsonTreeRoot = getJsonTreeRoot(parser);
         IRequestAction<?> action = deserializeAction(jsonTreeRoot);
+        logger.info(LOG_DEFAULT, "action", JacksonUtils.toYMLPrettyTry(action));
 
         return httpServiceRequest;
     }
 
-    private IRequestAction<?> deserializeAction(JsonNode root) {
+    private IRequestAction<?> deserializeAction(JsonNode root) throws JsonProcessingException {
 
         String actionName = root.get("action").get("name").asText();
-        logger.info("HTTP_REQUEST_ACTION_TYPE: {}", actionName);
-        return null;
+        HttpRequestActionTypeEnum actionType = HttpRequestActionTypeEnum.valueOf(actionName);
+        logger.info("HTTP_REQUEST_ACTION_TYPE: {}", actionType);
+
+        Class paramsClz = actionParamMap.get(actionType);
+        logger.info("PARAMS_CLASS: {}", paramsClz);
+
+        JsonNode paramsNode = root.get("action").get("params");
+        Object params = null;
+        try {
+            params = objectMapper.treeToValue(paramsNode, paramsClz);
+        } catch (JsonProcessingException e) {
+            logger.error(e.getMessage());
+            throw e;
+        }
+        return new IRequestAction(actionType, params);
     }
 
     private JsonNode getJsonTreeRoot(JsonParser parser) throws IOException {
