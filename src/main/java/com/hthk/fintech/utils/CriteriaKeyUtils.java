bae41802390a750c89bbc487c7d4aedbe824ed6c
rockchen <rochchen@htsc.com>
2024-01-05 15:12:54 +0800

    update

--- src/main/java/com/hthk/fintech/utils/CriteriaKeyUtils.java
+++ src/main/java/com/hthk/fintech/utils/CriteriaKeyUtils.java
@@ -2,6 +2,11 @@ package com.hthk.fintech.utils;
 
 import com.hthk.fintech.model.common.Criteria;
 import com.hthk.fintech.model.common.CriteriaKey;
+import com.hthk.fintech.model.web.http.ActionTypeEnum;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Author: Rock CHEN
@@ -9,12 +14,14 @@ import com.hthk.fintech.model.common.CriteriaKey;
  */
 public class CriteriaKeyUtils {
 
-    public static CriteriaKey build(Criteria criteria) {
-        return new CriteriaKey(
-                criteria.action(),
+    public static List<CriteriaKey> build(Criteria criteria) {
+
+        List<ActionTypeEnum> actionList = Arrays.stream(criteria.actions()).collect(Collectors.toList());
+        return actionList.stream().map(t -> new CriteriaKey(
+                t,
                 criteria.type(),
                 criteria.subType1(), criteria.subType2(), criteria.subType3(), criteria.subType4(), criteria.subType5(),
-                criteria.appName());
+                criteria.appName())).collect(Collectors.toList());
     }
 
 }
