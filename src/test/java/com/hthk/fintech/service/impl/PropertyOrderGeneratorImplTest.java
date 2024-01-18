5024ef5e91f433190dcea66e626420af51e72474
rockchen <rochchen@htsc.com>
2024-01-18 10:41:55 +0800

    update

--- src/test/java/com/hthk/fintech/service/impl/PropertyOrderGeneratorImplTest.java
+++ src/test/java/com/hthk/fintech/service/impl/PropertyOrderGeneratorImplTest.java
@@ -16,12 +16,12 @@ public class PropertyOrderGeneratorImplTest {
     public void testGenerate_JACKSON() {
 
         String nameSpace = "JsonPropertyOrder";
-//        String sourceFile = "C:/Rock/Datas/IT/DEV/ws/WorkSpaceIDEAHTSFICC/calypso-model/src/main/java/com/hthk/calypsox/model/trade/TradeInfo.java";
+        String sourceFile = "C:/Rock/Datas/IT/DEV/ws/WorkSpaceIDEAHTSFICC/calypso-model/src/main/java/com/hthk/calypsox/model/trade/TradeInfo.java";
 //        String sourceFile = "C:/Rock/Datas/IT/DEV/ws/WorkSpaceIDEAHTSFICC/calypso-model/src/main/java/com/hthk/calypsox/model/position/CashPositionInfo.java";
 //        String sourceFile = "C:/Rock/Datas/IT/DEV/ws/WorkSpaceIDEAHTSFICC/calypso-model/src/main/java/com/hthk/calypsox/model/staticdata/future/contract/FutureInfo.java";
 
 //        String sourceFile = "C:/Rock/Datas/IT/DEV/ws/WorkSpaceIDEAHTSFICC/calypso-model/src/main/java/com/hthk/calypsox/model/trade/product/FutureFXTradeInfo.java";
-        String sourceFile = "C:/Rock/Datas/IT/DEV/ws/WorkSpaceIDEAHTSFICC/calypso-model/src/main/java/com/hthk/calypsox/model/marketdata/quote/eod/EODQuote.java";
+//        String sourceFile = "C:/Rock/Datas/IT/DEV/ws/WorkSpaceIDEAHTSFICC/calypso-model/src/main/java/com/hthk/calypsox/model/marketdata/quote/eod/EODQuote.java";
         int offSet = 3;
 
         String output = generator.process(nameSpace, sourceFile, offSet);
